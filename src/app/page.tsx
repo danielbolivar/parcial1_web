2903d6b7edcca188d6dde2927a06f73a366d57d7
Daniel Bolivar <5009484+danielbolivar@users.noreply.github.com>
2025-09-23 17:52:20 -0500

    añadido contexto para manejar estado global (favoritos)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+'use client';
 import ListEpisodes from "@/components/ListEpisodes";
+import { useContext, useState, createContext } from "react";
+
+
+
+const FavoriteContext = createContext<any>(null);
 
 export default function Home() {
+
+  const [favorites, setFavorites] = useState<number[]>([]);
+
+  const addFavorite = (id: number) => {
+    setFavorites((prev) => [...prev, id]);
+  };
+
+  const removeFavorite = (id: number) => {
+    setFavorites((prev) => prev.filter((favId) => favId !== id));
+  };
+
   return (
-    <div className="">
-      <ListEpisodes />
-    </div>
+    <FavoriteContext value={{ favorites, addFavorite, removeFavorite }}>
+      <div className="container mx-auto p-4">
+        <ListEpisodes />
+      </div>
+    </FavoriteContext>
   );
 }
